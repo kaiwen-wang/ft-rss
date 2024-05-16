f2ce4cc7cf053aec4f77f64a10394d2bab0ae3b8
Kaiwen Wang <kvnwkw@gmail.com>
2024-05-16 01:51:37 -0400

    update

--- README.txt
+++ README.txt
@@ -1,1 +1,2 @@
-https://kaiwen-wang.github.io/quiz/
+https://kaiwen-wang.github.io/ft-rss/src/index.html
+CURRENTLY NOT WORKING! WORK IN PROGRESS!
--- scraper.js
+++ scraper.js
@@ -2,25 +2,19 @@ import * as cheerio from 'cheerio';
 import * as fs from 'fs';
 import RSS from 'rss';
 
-var feed = new RSS(
-    {
-        title: "FT Top Stories",
-        image_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/25/Financial_Times_corporate_logo_%28alternative%29.svg/1200px-Financial_Times_corporate_logo_%28alternative%29.svg.png",
-        site_url: "ft.com"
-    }
-);
+var feed = new RSS({
+    title: "FT Top Stories",
+    image_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/25/Financial_Times_corporate_logo_%28alternative%29.svg/1200px-Financial_Times_corporate_logo_%28alternative%29.svg.png",
+    site_url: "ft.com"
+});
 
 const url = 'https://ft.com';
 
 async function scrape() {
     const res = await fetch(url);
     const html = await res.text();
-
     const $ = cheerio.load(html);
-
-    const $mostReads = $('.o-header__mega-column--articles')
-    // console.log($mostReads.html());
-
+    const $mostReads = $('.o-header__mega-column--articles');
     const $frontRead = $('.list__items-wrapper').first();
 
     const today = new Date();
@@ -34,11 +28,8 @@ async function scrape() {
         timeZone: 'UTC',
         timeZoneName: 'short'
     };
-
-    // Round the time to the nearest 5 minutes
     today.setMinutes(Math.round(today.getMinutes() / 5) * 5);
     today.setSeconds(0, 0);
-
     const formattedDate = today.toLocaleString('en-US', options);
 
     let RSSElement = {
@@ -46,40 +37,33 @@ async function scrape() {
         "description": ""
     };
 
+    let mostReadHtml = '<ul class="list-decimal list-inside">';
+
     $frontRead.find('a').each((index, element) => {
         const href = $(element).attr('href');
         let text = $(element).text();
-        text = text.replace('opinion content.', '');
+        text = text.replace('opinion content.', '[Opinion]');
         const link = `<a href="${url}${href}">${text}</a>`;
         RSSElement.description += `${index + 1}. ${link}<br>`;
+        mostReadHtml += `<li class="text-blue-500 hover:text-blue-200">${link}</li>`;
     });
 
-    feed.item(RSSElement);
+    mostReadHtml += '</ul>';
 
+    const indexHtml = fs.readFileSync('./src/index.html', 'utf8');
+    const $index = cheerio.load(indexHtml);
+    $index('#most-read-list-container').empty();
+    $index('#most-read-list-container').prepend(mostReadHtml);
+    fs.writeFileSync('./src/index.html', $index.html());
+
+    feed.item(RSSElement);
     let xml = feed.xml();
     fs.writeFile("rss.xml", xml, function (err) {
         if (err) {
             return console.log(err);
         }
-        console.log("rss feed saved")
-    })
-
-    // const sections = ["World", "Companies", "Markets", "Opinion", "Work/Careers", "Life/Arts"]
-
-    // $mostReads.each((index, element) => {
-    //     const allText = $(element).text();
-    //     const lines = allText.split('\n');
-
-    //     lines.forEach((line, lineIndex) => {
-    //         const trimmedLine = line.trim();
-    //         if (trimmedLine) {
-    //             console.log(`Line ${lineIndex + 1}: ${trimmedLine}`);
-    //         }
-    //     });
-
-    //     console.log('\n'); // add a separator between elements
-    // });
-
+        console.log("rss feed saved");
+    });
 }
 
 scrape();
