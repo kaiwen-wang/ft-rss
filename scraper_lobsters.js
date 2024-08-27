70fbdcfc141d161cc5dac2be0306b4b99a4c370b
Kaiwen Wang <kvnwkw@gmail.com>
2024-08-27 13:39:07 -0400

    update .DS_Store, main.yml, scraper_hn.js and src/index.html

--- scraper_lobsters.js
+++ scraper_lobsters.js
@@ -3,12 +3,12 @@ import * as fs from 'fs';
 import RSS from 'rss';
 
 var feed = new RSS({
-    title: "Hacker News Top Stories",
-    image_url: "https://news.ycombinator.com/y18.svg",
-    site_url: "https://news.ycombinator.com/"
+    title: "Lobsters Top Stories",
+    image_url: "https://lobste.rs/apple-touch-icon-144.png",
+    site_url: "https://lobste.rs/"
 });
 
-const url = 'https://news.ycombinator.com/';
+const url = 'https://lobste.rs/';
 
 async function scrape() {
     const res = await fetch(url);
@@ -31,24 +31,21 @@ async function scrape() {
     const formattedDate = today.toLocaleString('en-US', options);
 
     let RSSElement = {
-        "title": "Hacker News Top Stories: " + formattedDate,
+        "title": "Lobsters Top Stories: " + formattedDate,
         "description": ""
     };
 
     let topStoriesHtml = '<ol class="list-decimal list-inside">';
 
-    $('.athing').each((index, element) => {
-        // if (index >= 30) return false; // Only get top 30 stories
-
-        const $title = $(element).find('.titleline > a');
+    $('.story').each((index, element) => {
+        const $title = $(element).find('.link a.u-url');
         const href = $title.attr('href');
         let text = $title.text();
-        const $subtext = $(element).next().find('.subtext');
-        const points = $subtext.find('.score').text();
-        const comments = $subtext.find('a').last().text();
+        const points = $(element).find('.score').text();
+        const comments = $(element).find('.comments_label a').text();
 
         const link = `<a href="${href}">${text}</a>`;
-        const details = `${points} | ${comments}`;
+        const details = `${points} points | ${comments}`;
 
         RSSElement.description += `${index + 1}. ${link} (${details})<br>`;
         topStoriesHtml += `<li class="text-blue-500 hover:text-blue-200">${link} <span class="text-gray-500">(${details})</span></li>`;
@@ -64,7 +61,7 @@ async function scrape() {
 
     feed.item(RSSElement);
     let xml = feed.xml();
-    fs.writeFile("rss_hn.xml", xml, function (err) {
+    fs.writeFile("rss_lobsters.xml", xml, function (err) {
         if (err) {
             return console.log(err);
         }
